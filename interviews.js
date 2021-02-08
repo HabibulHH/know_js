685b4fd4f299f149e8229dee10b7acb01f09eabd
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-02-08 21:08:13 +0600

    Added interview json questions

---
+++ interviews.js
@@ -0,0 +1,31 @@
+let Questions = [
+  {
+    question: "How many scopes are there in js",
+    answer:
+      "There are founr types of scope in js 1. Global,Function, Block, Module ",
+  },
+  {
+    question:
+      " var is function scope , let and const are block scope is it true",
+    answer: "yes",
+  },
+  {
+    question:
+      "What is object freezing? can we change object after freezing it? if properties have object value, e change inside the objects",
+    answer:
+      "Freezing is making an object immutable, we can not chnage or add new properties and change values, we can only change nested objects values",
+  },
+  {
+    question:
+      " We can not add new properties after sealing object , we only can chnage existing key values. Is it true",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+];
