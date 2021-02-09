7aef6ef99976e770a8e476d7e3a501f22c450c79
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-02-09 12:14:25 +0600

    React files added

---
+++ react_questions.js
@@ -0,0 +1,58 @@
+let Questions = [
+  {
+    question: "What is HOC",
+    answer: "",
+  },
+
+  {
+    question: "What are differeces in states and props",
+    answer: "",
+  },
+  {
+    question: "What are three phases of life cycle?",
+    answer: "Mounting, Updating, Unmounting",
+  },
+  {
+    question: "What are alternatives of Redux",
+    answer: "mobx,flux,appollo graphQL",
+  },
+  {
+    question: "What is synthetic event",
+    answer:
+      "A cross browser wrapper of native events to make sure react events are consistant accross  browser",
+  },
+  {
+    question: "What is ref in react",
+    answer: "Reference of a component of single element",
+  },
+  {
+    question: "What are uses of refs?",
+    answer:
+      "Third party DOM integration,Animations initiation, Media playback, focus",
+  },
+  {
+    question: "What are controlled components",
+    answer: "Componnets that have ability to maintain their states",
+  },
+  {
+    question: "What can be done when react component is slow",
+    answer:
+      "Use pure function, React.usememo uses can be used whereever necessery",
+  },
+  {
+    question: "What are different prop types in react",
+    answer: "Func , bool , node , string, number",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+];
